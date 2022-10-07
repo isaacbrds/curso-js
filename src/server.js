dd6621106b165a5d308488bed02dc0769ee08eca
isaacbrds <isaac.brigido@gmail.com>
2022-10-07 16:17:38 -0300

    feat: configuração da suite de testes

--- src/server.js
+++ src/server.js
@@ -1,13 +1,6 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import app from './app'
+const PORT = 3000
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-})
-
-app.listen(3000, ()=>{
-  console.log('App listening on port 3000');
-})
-
+app.listen(PORT, ()=>{
+  console.log('App running at port ' + PORT);
+});
