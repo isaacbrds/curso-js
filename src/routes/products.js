2af08eb9922da92a5fc5683f78400f08fc290cc3
isaacbrds <isaac.brigido@gmail.com>
2022-10-10 10:08:55 -0300

    feat: configurando controllers e testes unitários

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,13 +1,11 @@
 import express from 'express';
+import ProductsController from '../controllers/products'
 
 const router = express.Router();
+const productsController = new ProductsController();
 
 router.get('/', (req, res) => {
-  res.send([{ 
-    name: 'default product', 
-    description: 'product description', 
-    price: 100
-  }])
+  productsController.get(req,res);
 });
 
 export default router;
