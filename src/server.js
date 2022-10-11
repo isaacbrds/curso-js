b125cb699d83d0db1fb6f2fddb103ec35a8847c8
isaacbrds <isaac.brigido@gmail.com>
2022-10-11 09:22:25 -0300

    feat: configurando mongodb, docker e mongoose

--- src/server.js
+++ src/server.js
@@ -1,6 +1,29 @@
-import app from './app'
+import setupApp from './app'
 const PORT = 3000
 
-app.listen(PORT, ()=>{
-  console.log('App running at port ' + PORT);
-});
+(async () => {
+  try {
+    const app = await SetupApp();
+    const server = app.listen(PORT, ()=>{
+      console.log('App running at port ' + PORT);
+    });
+    
+    const exitSignals = ["SIGINT", "SIGTERM", "SIGQUIT"]
+    exitSignals.map(sig=>process.on(sig, ()=>{
+      server.close(err => {
+        if(err) {
+          console.error(err)
+          process.exit(1)
+        }
+        app.database.connection.close(function(){
+          console.info("Database connection closed!")
+          process.exit(0)
+        })
+
+      })
+    }))
+  }catch(err){
+    console.error(err)
+    process.exit(1)
+  }
+})();
